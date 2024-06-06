d7d5b287b6f01a44c2cfd722fdac81fbf0f00e9f
Roberto Escalon <roberto.escalon@applaudostudios.com>
2024-06-05 22:17:56 -0600

    🚚 develop - organize package names, create basic UI TabView.

---
+++ MoviesApp/Extensions/PreviewDataProvider.swift
@@ -0,0 +1,19 @@
+//
+//  PreviewProvider.swift
+//  MoviesApp
+//
+//  Created by rescalon on 5/6/24.
+//
+
+import Foundation
+import SwiftUI
+
+/*
+ This class will be hosting all preview complex data objects with dummy data.
+ */
+class PreviewDataProvider {
+    static let instance = PreviewDataProvider()
+    private init() { }
+    
+    let movie = Movie(id: 1, title: "Ape Planet")
+}
---
+++ MoviesApp/Model/Movie.swift
@@ -0,0 +1,13 @@
+//
+//  Movie.swift
+//  MoviesApp
+//
+//  Created by rescalon on 5/6/24.
+//
+
+import Foundation
+
+struct Movie: Identifiable {
+    let id: Int
+    let title: String
+}
