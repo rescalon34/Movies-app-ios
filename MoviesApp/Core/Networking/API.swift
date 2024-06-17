60dff707992e1eab9c6760796c05e93da1bf6f42
rescalon34 <roberto.escalon34@gmail.com>
2024-06-17 14:48:16 -0600

    ➕ feature/setup-network-calls - Add Moya dependency for Network API calls.

---
+++ MoviesApp/Core/Networking/API.swift
@@ -0,0 +1,35 @@
+//
+//  API.swift
+//  MoviesApp
+//
+//  Created by rescalon on 17/6/24.
+//
+
+import Foundation
+import Moya
+
+enum API {
+    
+}
+
+extension API: TargetType {
+    var baseURL: URL {
+        <#code#>
+    }
+    
+    var path: String {
+        <#code#>
+    }
+    
+    var method: Moya.Method {
+        <#code#>
+    }
+    
+    var task: Moya.Task {
+        <#code#>
+    }
+    
+    var headers: [String : String]? {
+        <#code#>
+    }
+}
